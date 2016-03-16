8f650823efebae3d06ee997bfd9dab475d69428b
antonmes <antonmes@users.noreply.github.com>
2016-03-16 09:48:01 +0300

    Remove C deps

--- Package.swift
+++ Package.swift
@@ -1,17 +1,9 @@
 import PackageDescription
 
-#if os(OSX)
-    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
-#else
-    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
-#endif
-
 let package = Package(
     name: "TCPSSL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
-        .Package(url: openSSLURL, majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 4),
     ]
 )

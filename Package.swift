e9127a812708976028be0157d9e678c3d4c52428
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-04-29 19:53:41 +0200

    Updated Swift 04-25 (#6)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TCPSSL",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 6),
     ]
 )
